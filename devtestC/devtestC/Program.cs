f56fb44563d200003e876ac17108b89fa09d9b6e
JeanLaurent <jlcassier@gmail.com>
2020-12-24 10:53:58 +0700

    init

---
+++ devtestC/devtestC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace devtestC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Gros!");
+
+            var monTruc = new MonTruc
+            {
+                A = 6,
+                B = 3,
+                C = A / B
+            };
+        }
+
+    class MonTruc {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+    }
+
+    }
+}
